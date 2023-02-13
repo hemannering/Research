2a21ca84ef56e620f7342395df0f6ed301666b63
hemannering <55000580+hemannering@users.noreply.github.com>
2023-02-13 12:28:10 -0500

    Create compute_containment.py

---
+++ scripts/compute_containment.py
@@ -0,0 +1,25 @@
+# Purpose: python script to compute the containment of genomes 
+# Name: Hannah Mannering 
+import os
+import glob
+from pathlib import Path
+
+# get fasta files
+# write what path you want to look at
+path = '/home/hemannering/DNA_Research/DNA_Research/subset/*.fna.gz'
+fasta_files = glob.glob(path)
+
+# get mash files
+# write what path you want to look at 
+path2 = '/home/hemannering/DNA_Research/DNA_Research/subset/*.fna.gz.msh'
+mash_files = glob.glob(path2)
+
+# iterate through fasta files
+for filename_fasta in fasta_files:
+    # check to ensure it is printing out fasta 
+    #print(filename_fasta)
+    for filename_mash in mash_files:
+        # check to ensure it is printing mash 
+        #print(filename_mash)
+        os.system('mash screen ' + filename_mash + ' ' + filename_fasta + ' | cut -f1,2 > cont_within_.txt')
+
